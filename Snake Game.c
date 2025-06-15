6745551b84564cf3b66c13deef40835a441d7a20
Wendell <josewendell.sa@gmail.com>
2025-06-15 11:57:50 -0300

    Adicionando hitbox no corpo, escolha de tamanho de mapa e compatibilidade parcial com linux

--- Snake Game.c
+++ Snake Game.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#ifdef _WIN32
+#define clear() clearW()
+#else
+#include "kbhitlinux.h"
+#define clear() clearL()
+#define kbhit() kbhitL()
+#endif
 
 const int SnakeInitialSize = 3;
-const int sizeX = 30, sizeY = 20;
+const int maxSizeX = 50, maxSizeY = 50; // minimum is 10
+int sizeX = 30, sizeY = 20; //Default map size config
 
 typedef struct Segment
 {
@@ -27,6 +35,28 @@ typedef struct
 	int size;
 }Snake;
 
+void clearW()
+{
+	system("cls");
+}
+
+void clearL()
+{
+	system("clear");
+}
+
+Snake* CreateSnake()
+{
+	Snake* snake = malloc(sizeof(Snake));
+	snake->head = NULL;
+	snake->tail = NULL;
+	snake->size = 0;
+	snake->dirX = 1;
+	snake->dirY = 0;
+	
+	return snake;
+}
+
 void AddSegment(Snake* snake)
 {
 	Segment* newSegment = malloc(sizeof(Segment));
@@ -49,26 +79,57 @@ void InitializeSnake(Snake* snake)
 	for (n = 0 ; n < SnakeInitialSize ; n++)
 	{
 		AddSegment(snake);
-		snake->tail->x = 5-n;
-		snake->tail->y = 4;
+		snake->tail->x = sizeX/2-n;
+		snake->tail->y = sizeY/2;
 	}
 }
 
-Snake* CreateSnake()
+void ResetSnake(Snake* snake)
 {
-	Snake* snake = malloc(sizeof(Snake));
+	Segment* aux = snake->head;
+	
+	while (aux->next != NULL)
+	{
+		Segment* aux2 = aux;
+		aux = aux->next;
+		free(aux2);
+	}
+	
+	free(aux);
+	
 	snake->head = NULL;
 	snake->tail = NULL;
 	snake->size = 0;
+}
+
+void ResetMap(Snake* snake, char map[maxSizeY][maxSizeX])
+{
+	int n, m;
+
+	for (n = 0 ; n < sizeY ; n++)
+	{
+		for (m = 0 ; m < sizeX ; m++)
+		{
+			map[n][m] = ' ';
+		}
+	}
+	
+	if (snake->size > 0)
+		ResetSnake(snake);
+	InitializeSnake(snake);
 	snake->dirX = 1;
 	snake->dirY = 0;
 	
-	InitializeSnake(snake);
+	Segment* aux = snake->head;
 	
-	return snake;
+	for (n = 0 ; n < snake->size ; n++)
+	{
+		map[aux->y][aux->x] = 'O';
+		aux = aux->next;
+	}
 }
 
-void SpawnFruit(char map[sizeY][sizeX])
+void SpawnFruit(char map[maxSizeY][maxSizeX])
 {
 	FRUIT.x = rand() % sizeX;
 	FRUIT.y = rand() % sizeY;
@@ -82,66 +143,20 @@ void SpawnFruit(char map[sizeY][sizeX])
 	map[FRUIT.y][FRUIT.x] = '*';
 }
 
-void MoveSnake(Snake* snake, char input, char map[sizeY][sizeX])
+int MoveSnake(Snake* snake, char map[maxSizeY][maxSizeX])
 {
 	Segment* aux = snake->head->next;
 	
 	int auxX, auxY;
 	int auxX2 = snake->head->x, auxY2 = snake->head->y;
 	
-	if (input > 'Z')
-	{
-		input -= 32;
-	}
-	
-	switch(input)
-	{
-		case 'W':
-			if (snake->dirY == 1)
-				goto YouCantDoThis;
-			else
-			{
-				snake->dirY = -1;
-				snake->dirX = 0;
-				snake->head->y -= 1;
-			}
-			break;
-		case 'A':
-			if (snake->dirX == 1)
-				goto YouCantDoThis;
-			else
-			{
-				snake->dirY = 0;
-				snake->dirX = -1;
-				snake->head->x -= 1;
-			}
-			break;
-		case 'S':
-			if (snake->dirY == -1)
-				goto YouCantDoThis;
-			else
-			{
-				snake->dirY = 1;
-				snake->dirX = 0;
-				snake->head->y += 1;
-			}
-			break;
-		case 'D':
-			if (snake->dirX == -1)
-				goto YouCantDoThis;
-			else
-			{
-				snake->dirY = 0;
-				snake->dirX = 1;
-				snake->head->x += 1;
-			}
-			break;
-	}
+	snake->head->x += snake->dirX;
+	snake->head->y += snake->dirY;
 	
 	if (((snake->head->x < 0 || snake->head->x >= sizeX) || (snake->head->y < 0 || snake->head->y >= sizeY)) || map[snake->head->y][snake->head->x] == 'O')
 	{
-		printf("Voce perdeu!\n");
-		return;
+		printf("\nVoce perdeu!\n");
+		return 0;
 	}
 
 	while (aux != NULL)
@@ -169,19 +184,20 @@ void MoveSnake(Snake* snake, char input, char map[sizeY][sizeX])
 		
 		map[snake->tail->y][snake->tail->x] = 'O';
 		
+		if (snake->size == sizeX*sizeY)
+		{
+			return 2;
+		}
+		
 		SpawnFruit(map);
 	}
 	
 	map[snake->head->y][snake->head->x] = 'O';
 	
-	return;
-	
-	YouCantDoThis:
-	
-	printf("Nao pode ir na direcao oposta a atual!\n");
+	return 1;
 }
 
-void ShowMap(char map[sizeY][sizeX])
+void ShowMap(char map[maxSizeY][maxSizeX])
 {
 	int x, y;
 	
@@ -199,48 +215,99 @@ void ShowMap(char map[sizeY][sizeX])
 	}
 }
 
+void UpdateSnakeDirection(char input, Snake* snake)
+{
+    switch(input)
+	{
+		case 'w':
+			if (snake->dirY == 0)
+			{
+				snake->dirY = -1;
+				snake->dirX = 0;
+			}
+			break;
+		case 'a':
+			if (snake->dirX == 0)
+			{
+				snake->dirY = 0;
+				snake->dirX = -1;
+			}
+			break;
+		case 's':
+			if (snake->dirY == 0)
+			{
+				snake->dirY = 1;
+				snake->dirX = 0;
+			}
+			break;
+		case 'd':
+			if (snake->dirX == 0)
+			{
+				snake->dirY = 0;
+				snake->dirX = 1;
+			}
+			break;
+	}
+}
+
+void GetKeyDown(Snake* snake)
+{
+	char ch = kbhit();
+    if (ch > 0)
+    {
+    	if (ch < 2)
+    		ch = getch();
+    	UpdateSnakeDirection(ch, snake);
+    }
+}
+
 void main()
 {
 	srand(time(NULL));
 	
 	Snake* snake = CreateSnake();
-	char map[sizeY][sizeX];
+	char map[maxSizeY][maxSizeX];
 	
 	int n, m;
 	
-	for (n = 0 ; n < sizeY ; n++)
+	char Game = 1;
+	char userWantsToPlay = 1;
+	
+	printf("Ola!\n");
+	
+	while (userWantsToPlay)
 	{
-		for (m = 0 ; m < sizeX ; m++)
-		{
-			map[n][m] = ' ';
-		}
-	}
-	
-	Segment* aux = snake->head;
-	
-	for (n = 0 ; n < snake->size ; n++)
-	{
-		map[aux->y][aux->x] = 'O';
-		aux = aux->next;
-	}
-	
-	SpawnFruit(map);
-	
-	char input, enterCollector;
-	
-	while (1)
-	{
-		ShowMap(map);
+		printf("Informe o tamanho do mapa que deseja:\nO tamanho maximo eh %dx%d e o minimo eh 10x10\nEscolha o tamanho x e y: ", maxSizeX, maxSizeY);
+		scanf("%d %d", &sizeX, &sizeY);
+		if (sizeX < 10) sizeX = 10;
+		else if (sizeX > maxSizeX) sizeX = maxSizeX;
+		if (sizeY < 10) sizeY = 10;
+		else if (sizeY > maxSizeY) sizeY = maxSizeY;
 		
-		printf("\n\n\n W \nASD\n\nEscolha o proximo movimento: ");
+		int timer = 5;
+		printf("Muito bem!\nConfigurando mapa...");
+		ResetMap(snake, map);
+		SpawnFruit(map);
+		sleep(1);
+		printf(" Iniciando o jogo em:\n");
+		while (timer > 0)
+		{
+			printf("%d ", timer);
+			sleep(1);
+			timer -= 1;
+		}
+		
 		do
 		{
-			scanf("%c", &input);
-		}while (input == '\n');
+			clear();
+			ShowMap(map);
+			usleep(150000);
+			GetKeyDown(snake);
+			Game = MoveSnake(snake, map);
+		}while (Game == 1);
 		
-		scanf("%c", &enterCollector);
-		
-		MoveSnake(snake, input, map);
+		if (Game == 2)
+			printf("\n\nParabens! Voce venceu!\n\n");
+		printf("\nPontuacao final: %d\n", snake->size - SnakeInitialSize);
 	}
-	
 }
